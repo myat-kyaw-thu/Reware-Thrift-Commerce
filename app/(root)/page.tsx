11237d15600bbd4dcc2180a857976113d3ffb8b2
Andrew <myatkyawthu4002@gmail.com>
2025-05-19 21:38:19 +0630

    Applying Proejct Card in

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,10 @@
-
+import sampleData from "@/db/sample-data";
+import ProductList from "../../components/shared/product/product-list";
 export default function Home() {
   return (
+    <>
+     <ProductList data={sampleData.products} title="Newest Arrials" />
       <h1>Hello World</h1>
+    </>
   );
 }
