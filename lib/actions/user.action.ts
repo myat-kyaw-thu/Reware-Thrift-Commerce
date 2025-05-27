9be56ea8f66a1f3c7893fa5ca1994c7e32d110df
Andrew <myatkyawthu4002@gmail.com>
2025-05-27 21:05:22 +0630

    signUpUser

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -48,3 +48,37 @@ export async function signOutUser() {
   }
   await signOut();
 }
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword'),
+    });
+
+    const plainPassword = user.password;
+
+    user.password = await hash(user.password);
+
+    await prisma.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: user.password,
+      },
+    });
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword,
+    });
+
+    return { success: true, message: 'User registered successfully' };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { success: false, message: formatError(error) };
+  }
+}
