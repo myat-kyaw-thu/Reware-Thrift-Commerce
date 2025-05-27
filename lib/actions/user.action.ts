184543c534dd9e69149d22fe16178c819b301710
Andrew <myatkyawthu4002@gmail.com>
2025-05-27 21:05:58 +0630

    updateUserAddress

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -91,3 +91,29 @@ export async function getUserById(userId: string) {
   if (!user) throw new Error('User not found');
   return user;
 }
+
+export async function updateUserAddress(data: ShippingAddress) {
+  try {
+    const session = await auth();
+
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+
+    if (!currentUser) throw new Error('User not found');
+
+    const address = shippingAddressSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { address },
+    });
+
+    return {
+      success: true,
+      message: 'User updated successfully',
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
