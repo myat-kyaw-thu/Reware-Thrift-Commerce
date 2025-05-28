ef050d5b32cfa7cb1ae3f8001bf503041eb4bbbf
Andrew <myatkyawthu4002@gmail.com>
2025-05-28 20:01:54 +0630

    Update layout.tsx

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,5 +3,5 @@ export default function AuthLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className='flex-center min-h-screen w-full'>{children}</div>;
+  return <div>{children}</div>;
 }
