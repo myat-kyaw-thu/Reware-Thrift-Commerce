c40426f4ecf443ce698b2f744f9c68e69df408d3
Andrew <myatkyawthu4002@gmail.com>
2025-06-02 20:01:51 +0630

    calculate Price Funtion

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,25 +1,20 @@
 "use server"
 
-import { cookies } from "next/headers"
 import { CartItem } from "../../types"
-import { prisma } from "@/db/prisma"
-import {auth} from "@/auth"
-import { cartItemSchema, insertCartSchema } from '../validators';
-import { revalidatePath } from 'next/cache';
-import { Prisma } from '@prisma/client';
+import { round2 } from "../utils"
 
 const calcPrice = (items: CartItem[]) => {
   const itemsPrice = round2(
     items.reduce((acc, item) => acc + Number(item.price) * item.qty, 0)
   ),
-  shippingPrice =
-}
-export function round2(value: number | string) {
-  if (typeof value === 'number') {
-    return Math.round((value + Number.EPSILON) * 100) / 100;
-  } else if (typeof value === 'string') {
-    return Math.round((Number(value) + Number.EPSILON) * 100) / 100;
-  } else {
-    throw new Error('Value is not a number or string');
+  shippingPrice = round2(itemsPrice > 100 ? 0 : 10),
+  taxPrice = round2(itemsPrice * 0.15),
+  totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+
+  return {
+    itemsPrice: itemsPrice.toFixed(2),
+    shippingPrice: shippingPrice.toFixed(2),
+    taxPrice: taxPrice.toFixed(2),
+    totalPrice: totalPrice.toFixed(2),
   }
 }
