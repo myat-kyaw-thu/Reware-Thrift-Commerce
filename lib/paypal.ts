eef31af8909cbb75153e2abfa1453bfa0ba5e163
Andrew <myatkyawthu4002@gmail.com>
2025-07-14 22:33:54 +0630

    handleResponse

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,1 +1,10 @@
 const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
+
+async function handleResponse(response: Response) {
+  if (response.ok) {
+    return response.json();
+  } else {
+    const errorMessage = await response.text();
+    throw new Error(errorMessage);
+  }
+}
