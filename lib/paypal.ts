824957ab044c1eabba4b7f9fdbd9e10bcb08a629
Andrew <myatkyawthu4002@gmail.com>
2025-07-14 22:34:07 +0630

    generateAccessToken

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,5 +1,26 @@
 const base = process.env.PAYPAL_API_URL || 'https://api-m.sandbox.paypal.com';
 
+
+async function generateAccessToken() {
+  const { PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET } = process.env;
+  const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString(
+    'base64'
+  );
+
+  const response = await fetch(`${base}/v1/oauth2/token`, {
+    method: 'POST',
+    body: 'grant_type=client_credentials',
+    headers: {
+      Authorization: `Basic ${auth}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  const jsonData = await handleResponse(response);
+  return jsonData.access_token;
+}
+
+
 async function handleResponse(response: Response) {
   if (response.ok) {
     return response.json();
