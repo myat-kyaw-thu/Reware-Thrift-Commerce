1f76a4a189ff52f89fbdc444a59538b48448bf5e
Andrew <myatkyawthu4002@gmail.com>
2025-07-18 22:27:46 +0630

    getMyOrders

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -9,6 +9,7 @@ import { convertToPlainObject, formatError } from '../utils';
 import { insertOrderSchema } from '../validators';
 import { getMyCart } from './cart.actions';
 import { getUserById } from './user.action';
+import { PAGE_SIZE } from '../constants';
 
 
 
@@ -196,3 +197,35 @@ export async function updateOrderToPaid({
     });
   });
 }
+
+export async function getMyOrders({
+  limit = PAGE_SIZE,
+  page,
+}: {
+  limit?: number;
+  page: number;
+}) {
+  const session = await auth();
+  if (!session) throw new Error('User is not authorized');
+
+  const data = await prisma.order.findMany({
+    where: { userId: session?.user?.id },
+    orderBy: { createdAt: 'desc' },
+    take: limit,
+    skip: (page - 1) * limit,
+  });
+
+  const dataCount = await prisma.order.count({
+    where: { userId: session?.user?.id },
+  });
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
+}
+
+type SalesDataType = {
+  month: string;
+  totalSales: number;
+}[];
