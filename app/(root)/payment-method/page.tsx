72342170a92bfc95d1e1591a3ad9a7addc0fcaee
Andrew <myatkyawthu4002@gmail.com>
2025-07-20 15:47:43 +0630

    Payment Graphite Colors and UI Refrom

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -1,26 +1,25 @@
-import { auth } from '@/auth';
-import CheckoutSteps from '@/components/shared/checkout-steps';
-import { getUserById } from '@/lib/actions/user.action';
-import { Metadata } from 'next';
-import PaymentMethodForm from './payment-method-form';
+import { auth } from "@/auth";
+import CheckoutSteps from "@/components/shared/checkout-steps";
+import { getUserById } from "@/lib/actions/user.action";
+import type { Metadata } from "next";
+import PaymentMethodForm from "./payment-method-form";
 
 export const metadata: Metadata = {
-  title: 'Select Payment Method',
+  title: "Select Payment Method",
 };
 
 const PaymentMethodPage = async () => {
   const session = await auth();
   const userId = session?.user?.id;
-
-  if (!userId) throw new Error('User not found');
+  if (!userId) throw new Error("User not found");
 
   const user = await getUserById(userId);
 
   return (
-    <>
+    <div className="min-h-screen bg-background">
       <CheckoutSteps current={2} />
       <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
-    </>
+    </div>
   );
 };
 
