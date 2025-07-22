1b3cbfd71147d02dac1339380cd286006d6b6576
Andrew <myatkyawthu4002@gmail.com>
2025-07-22 19:28:55 +0630

    shopping address Component Graphite Colors and UI Refrom

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -1,34 +1,31 @@
-import { auth } from '@/auth';
-import CheckoutSteps from '@/components/shared/checkout-steps';
-import { getMyCart } from '@/lib/actions/cart.actions';
-import { getUserById } from '@/lib/actions/user.action';
-import { ShippingAddress } from '@/types';
-import { Metadata } from 'next';
-import { redirect } from 'next/navigation';
-import ShippingAddressForm from './shipping-address-form';
+import { auth } from "@/auth";
+import CheckoutSteps from "@/components/shared/checkout-steps";
+import { getMyCart } from "@/lib/actions/cart.actions";
+import { getUserById } from "@/lib/actions/user.action";
+import type { ShippingAddress } from "@/types";
+import type { Metadata } from "next";
+import { redirect } from "next/navigation";
+import ShippingAddressForm from "./shipping-address-form";
 
 export const metadata: Metadata = {
-  title: 'Shipping Address',
+  title: "Shipping Address",
 };
 
 const ShippingAddressPage = async () => {
   const cart = await getMyCart();
-
-  if (!cart || cart.items.length === 0) redirect('/cart');
+  if (!cart || cart.items.length === 0) redirect("/cart");
 
   const session = await auth();
-
   const userId = session?.user?.id;
-
-  if (!userId) throw new Error('No user ID');
+  if (!userId) throw new Error("No user ID");
 
   const user = await getUserById(userId);
 
   return (
-    <>
+    <div className="min-h-screen bg-background">
       <CheckoutSteps current={1} />
       <ShippingAddressForm address={user.address as ShippingAddress} />
-    </>
+    </div>
   );
 };
 
