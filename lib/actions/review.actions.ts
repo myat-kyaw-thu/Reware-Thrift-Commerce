da1c0b7b22cf4ffddf4c9db2bf0d258d6e9299d6
Andrew <myatkyawthu4002@gmail.com>
2025-07-25 20:50:39 +0630

    getReviews

--- lib/actions/review.actions.ts
+++ lib/actions/review.actions.ts
@@ -82,3 +82,24 @@ export async function createUpdateReview(
     return { success: false, message: formatError(error) };
   }
 }
+
+
+export async function getReviews({ productId }: { productId: string; }) {
+  const data = await prisma.review.findMany({
+    where: {
+      productId: productId,
+    },
+    include: {
+      user: {
+        select: {
+          name: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return { data };
+}
