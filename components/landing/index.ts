66538631ed197f1d446647b0d2c4983b29f3e922
Andrew <myatkyawthu4002@gmail.com>
2025-08-31 17:33:06 +0630

    Exporting all the things from Index

---
+++ components/landing/index.ts
@@ -0,0 +1,8 @@
+export { BrandSlider } from './brand-slider';
+export { ContactSection } from './contact-section';
+export { FeaturesSection } from './features-section';
+export { Footer } from './footer';
+export { Hero } from './hero';
+export { Navbar } from './navbar';
+export { ProductsSection } from './products-section';
+
